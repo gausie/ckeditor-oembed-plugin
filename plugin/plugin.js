717927b80242958920cc75cd438ead5ca31b5321
Gausie <git@gaus.ie>
2014-01-22 16:14:50 +0000

    Fixed breaking on SCAYT

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,77 +1,88 @@
 ( function() {
 
-	'use strict';
+  'use strict';
 
-	CKEDITOR.plugins.add( 'oembed', {
+  CKEDITOR.plugins.add( 'oembed', {
 
-		requires: 'widget,dialog',
-		icons: 'oembed',
-		hidpi: true,
+    requires: 'widget,dialog',
+    icons: 'oembed',
+    hidpi: true,
 
-		onLoad: function() {
-			CKEDITOR.addCss('.oembed { background: rgb(225, 225, 225); border: 1px solid rgb(182, 182, 182); height: 100px; text-align: center; line-height: 100px; overflow: hidden; whitespace: nowrap; }');
-		},
+    onLoad: function() {
+      CKEDITOR.addCss('.oembed { background: rgb(225, 225, 225); border: 1px solid rgb(182, 182, 182); height: 100px; text-align: center; line-height: 100px; overflow: hidden; whitespace: nowrap; }');
+    },
 
-		init: function( editor ) {
+    init: function( editor ) {
 
-			var cls = editor.config.oembedClass || 'oembed';
+      var cls = editor.config.oembedClass || 'oembed';
 
 
-			editor.ui.addButton('oembed', {
-				label : Drupal.t('Embed'),
-				command : 'oembed'
-			});
+      editor.ui.addButton('oembed', {
+        label : Drupal.t('Embed'),
+        command : 'oembed'
+      });
 
-			editor.widgets.add( 'oembed', {
-				inline: false,
-				dialog: 'oembed',
-				mask: true, // prevent clickable
-				allowedContent: 'p(!' + cls + ')',
-				pathName: 'oembed',
+      editor.widgets.add( 'oembed', {
+        inline: false,
+        dialog: 'oembed',
+        mask: true, // prevent clickable
+        allowedContent: 'p(!' + cls + ')',
+        pathName: 'oembed',
 
-				template: '<p class="' + cls + '" data-cke-survive=1></p>',
+        template: '<p class="' + cls + '" data-cke-survive=1></p>',
 
-				parts: {
-					div: 'p'
-				},
+        parts: {
+          div: 'p'
+        },
 
-				defaults: {
-					url: ''
-				},
+        defaults: {
+          url: ''
+        },
 
-				data: function() {
-					this.element.$.innerHTML = Drupal.t('Embedding media from: ') + this.data.url;
-				},
+        data: function() {
+          this.element.$.innerHTML = Drupal.t('Embedding media from: ') + this.data.url;
+        },
 
-				upcast: function( el, data ) {
-					
-					if ( !( el.name == 'p' && el.hasClass( cls ) ) )
-						return;
+        upcast: function( el, data ) {
+          
+          if ( !( el.name == 'p' && el.hasClass( cls ) ) ){
+              return;
+          }
 
-					if ( el.children.length > 1 )
-						return;
+          if ( el.children.length > 1 ){
+              return;
+          }
 
-					data.url = el.children[0].value.replace(/\[embed\]([^\[]+)\[\/embed\]/i, '$1');
-					el.children[0].value = data.url;
+          data.url = el.children[0].value.replace(/\[embed\]([^\[]+)\[\/embed\]/i, '$1');
+          el.children[0].value = data.url;
 
-					// Add attribute to prevent deleting empty span in data processing.
-					el.attributes[ 'data-cke-survive' ] = 1;
+          // Add attribute to prevent deleting empty span in data processing.
+          el.attributes[ 'data-cke-survive' ] = 1;
 
-					return el;
-				},
+          return el;
+        },
 
-				downcast: function( el ) {
+        downcast: function( el ) {
+          
+          if( el.children.length > 1 ){
+              // Delete spellcheck tags
+              el.children.forEach(function(child, index){
+                  if(child.attributes !== undefined && child.attributes.hasOwnProperty('data-scaytid')){
+                      el.children.splice(index, 1);
+                 }
+              });
+          }
 
-					el.children[0].value = "[embed]"+this.data.url+"[/embed]";
+          el.children[0].value = "[embed]"+this.data.url+"[/embed]";
 
-					return el;
-				}
-			} );
+          return el;
+        }
+      } );
 
-			// Add dialog.
-			CKEDITOR.dialog.add( 'oembed', this.path + 'dialogs/oembed.js' );
+      // Add dialog.
+      CKEDITOR.dialog.add( 'oembed', this.path + 'dialogs/oembed.js' );
 
-		}
-	} );
+    }
+  } );
 
 } )();
